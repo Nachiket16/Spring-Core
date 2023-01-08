574b3fab66830e1d47c04453391183c5e96718bb
nachiket.khule <nachiketkhule.nk@gmail.com>
2023-01-08 15:24:58 +0530

    Spring core project added

---
+++ SpringCoreConcepts/src/test/java/com/nkspring/core/SpringCoreConcepts/SpringCoreConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkspring.core.SpringCoreConcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
